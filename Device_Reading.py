9de756c515fc593f041b2262429d7d6df0fadfea
Abiola-Agboola <163073055+Abiola-Agboola@users.noreply.github.com>
2024-04-04 14:20:14 +0100

    Device_Reading.py

--- Device_Reading.py
+++ Device_Reading.py
@@ -5,9 +5,9 @@ import os
 
 # Add navigation links to the four pages
 st.title("")
-st.markdown("[Go to Exercise Page](https://pcos-app-pcos-exercise.streamlit.app/)")
-st.markdown("[Go to PCOS Predictor Page](https://pcos-app-pcos-predictor.streamlit.app/)")
-st.markdown("[Go to Home Page](https://pcos-app-home.streamlit.app/)")
+st.markdown("[Exercise](https://pcos-app-pcos-exercise.streamlit.app/)")
+st.markdown("[PCOS Predictor](https://pcos-app-pcos-predictor.streamlit.app/)")
+st.markdown("[Home](https://pcos-app-home.streamlit.app/)")
 
 # Load the data and model
 exercise_data = pd.read_csv('cleaned_features.csv', encoding='ISO-8859-1')
